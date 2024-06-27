34e8b975a0471304245ae34280f46882e1a398a9
LeaCavalli <lea.cavalli01@gmail.com>
2024-06-27 09:53:48 +0200

    link active corrigé

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import '../src/style/style.css';
 
 const Header=() =>{
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname === path ? "nav-link active" : "nav-link";
+    };
+
     return (
         <div className="NavBar">
              <nav className="navbar navbar-expand-lg navbar-dark p-3 text-uppercase" id="navbar">
                 <div className="container-fluid d-flex">
                     <div className="row row-cols-2">
                         <div className="col">
-                            <a className="navbar-brand" href="#">John Doe</a>
+                            <Link className="navbar-brand" to="/">John Doe</Link>
                         </div>
                         <div className="col ms-auto d-flex justify-content-end">
                             <button className="navbar-toggler"  data-mdb-right="true" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,19 +27,19 @@ const Header=() =>{
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/">Accueil</a>
+                                    <Link className={isActive("/")} to="/">Accueil</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/Service">Services</a>
+                                    <Link className={isActive("/Service")} to="/Service">Services</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/Portfolio">Réalisations</a>
+                                    <Link className={isActive("/Portfolio")} to="/Portfolio">Réalisations</Link>
                                 </li>
                                 <li className="nav-item"> 
-                                    <a className="nav-link" href="/Blog">Blog</a>
+                                    <Link className={isActive("/Blog")} to="/Blog">Blog</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/Contact">Me contacter</a>
+                                    <Link className={isActive("/Contact")} to="/Contact">Me contacter</Link>
                                 </li>
                             </ul>
                         </div>
